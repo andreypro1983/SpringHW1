7f9ecdf3d1d1915ebf1ab75be1cd8bd9e5498bdc
Prozhikin_Andrey <a.prozhikin@gmail.com>
2024-01-30 22:09:45 +0200

    add class Person and pom.xml into repository

--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -1,9 +1,34 @@
 package ru.geekbrains;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//Домашнее задание
+//        Условие:
+//        Создать проект с использованием Maven или Gradle, добавить в него несколько зависимостей и написать код, использующий эти зависимости.
+//        Пример решения:
+//        1. Создайте новый Maven или Gradle проект, следуя инструкциям из блока 1 или блока 2.
+//        2. Добавьте зависимости org.apache.commons:commons-lang3:3.12.0 и com.google.code.gson:gson:2.8.6.
+//        3. Создайте класс Person с полями firstName, lastName и age.
+//        4. Используйте библиотеку commons-lang3 для генерации методов toString, equals и hashCode.
+//        5. Используйте библиотеку gson для сериализации и десериализации объектов класса Person в формат JSON.
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.geekbrains.model.Person;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Person person = new Person("Dart", "Wader",100);
+        System.out.println(person);
+
+        // сериализация с использованием Gson
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+        String gsonPerson = gson.toJson(person);
+        System.out.println("Результат сериализации: " + gsonPerson);
+
+        //десериализация с использованием Gson
+        Person resultPerson = gson.fromJson(gsonPerson,Person.class);
+        System.out.println("Результат десериализации: " + resultPerson);
     }
 }
--- src/main/java/ru/geekbrains/model/Person.java
+++ src/main/java/ru/geekbrains/model/Person.java
@@ -1,9 +1,49 @@
 package ru.geekbrains.model;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class Person {
     private String firstName;
     private String lastName;
     private int age;
 
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Person person)) return false;
+
+        return new EqualsBuilder()
+                .append(lastName, person.lastName)
+                .append(firstName, person.firstName)
+                .append(age, person.age)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(firstName)
+                .append(lastName)
+                .append(age)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("firstName", firstName)
+                .append("lastName", lastName)
+                .append("age", age)
+                .toString();
+    }
 }
